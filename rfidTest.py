41b5956b97830fcee965f14d430141d649e40a80
Akashi Chhaya <chhayaakashi@gmail.com>
2025-07-04 15:27:37 -0400

    testing

--- rfidTest.py
+++ rfidTest.py
@@ -1,1 +1,12 @@
-print("hello world")
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
+
+reader = SimpleMFRC522()
+
+try:
+    print("Place your RFID tag near the reader...")
+    id, text = reader.read()
+    print(f"ID: {id}")
+    print(f"Text on tag: {text}")
+finally:
+    GPIO.cleanup()
