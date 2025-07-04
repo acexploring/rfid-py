b573b43a0860bcf88dc55be99e22cef61e263650
Akashi Chhaya <chhayaakashi@gmail.com>
2025-07-04 16:01:04 -0400

    while loop

--- rfidTest.py
+++ rfidTest.py
@@ -1,12 +1,15 @@
 import RPi.GPIO as GPIO
+import time
 from mfrc522 import SimpleMFRC522
 
 reader = SimpleMFRC522()
 
 try:
-    print("Place your RFID tag near the reader...")
-    id, text = reader.read()
-    print(f"ID: {id}")
-    print(f"Text on tag: {text}")
+    while True:
+        print("Place your RFID tag near the reader...")
+        id, text = reader.read()
+        print(f"ID: {id}")
+        print(f"Text on tag: {text}")
+        time.sleep(1)  # Wait for a second before the next read
 finally:
     GPIO.cleanup()
